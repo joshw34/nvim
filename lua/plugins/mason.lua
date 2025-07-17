1ab57b6e781e7466546275426485c8aa80cbbb24
joshw34 <joshwhitley@msn.com>
2025-07-18 01:34:15 +0200

    removed mason-lspconfig
    it was starting a default instance of lsps before nvim-lspconfig started
    the custom version. having 2 instances of the same lsp cuased issues

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,15 +6,6 @@ return {
     end,
   },
   {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "clangd", "lua_ls" },
-        automatic_setup = false,
-      })
-    end,
-  },
-  {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
