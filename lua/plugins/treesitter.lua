adc84d74f40dcb2eb132873d9256f47de121a58a
joshw34 <joshwhitley@msn.com>
2025-08-04 12:11:19 +0200

    added python to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "c", "cpp", "lua" },
+      ensure_installed = { "c", "cpp", "lua", "python" },
       highlight = {
         enable = true,
       },
