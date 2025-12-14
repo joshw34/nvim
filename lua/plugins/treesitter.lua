976aefb9e5570969be15c74528c2815252c64753
joshw34 <joshwhitley@msn.com>
2025-12-14 15:32:17 +0100

    treesitter update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,10 +2,28 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "c", "cpp", "lua", "python", "dockerfile", "nginx", "go", "html" },
+    require("nvim-treesitter").setup({
+      ensure_installed = {
+        "bash",
+        "zsh",
+        "c",
+        "cpp",
+        "lua",
+        "python",
+        "dockerfile",
+        "nginx",
+        "go",
+        "html",
+        "vim",
+        "vimdoc",
+        "query",
+        "nix",
+      },
+      sync_install = false,
+      auto_install = true,
       highlight = {
         enable = true,
+        additional_vim_regex_highlighting = false,
       },
       indent = {
         enable = true,
