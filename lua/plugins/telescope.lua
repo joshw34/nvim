ac53968dc6a55953910e90a9cc3e343e9dd99ed1
joshw34 <joshwhitley@msn.com>
2025-12-14 20:32:29 +0100

    updated to newest telescope
    needed to be compatible with new treesitter

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,12 @@
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.8",
+  tag = 'v0.2.0',
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     require("telescope").setup({})
-    
+
     local builtin = require("telescope.builtin")
-    
+
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files in current directory" })
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep files in current directory" })
 
