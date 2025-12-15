faaeca4822bc6978844e233ecbb3e5c815df6fce
joshw34 <joshw34@joshw34.com>
2025-12-15 10:42:31 +0000

    fixed new treesitter setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,32 +2,34 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter").setup({
-      ensure_installed = {
-        "bash",
-        "zsh",
-        "c",
-        "cpp",
-        "lua",
-        "python",
-        "dockerfile",
-        "nginx",
-        "go",
-        "html",
-        "vim",
-        "vimdoc",
-        "query",
-        "nix",
-      },
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-      },
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = '*',
+      callback = function()
+        local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+        if not lang then
+          return
+        end
+
+        -- Check if already installed
+        local installed = vim.tbl_contains(
+          require('nvim-treesitter.info').installed_parsers(),
+          lang
+        )
+
+        -- Check if parser is available for installation
+        local available = vim.tbl_contains(
+          require('nvim-treesitter.parsers').available_parsers(),
+          lang
+        )
+
+        -- Only install if available and not installed
+        if available and not installed then
+          vim.cmd("TSInstall " .. lang)
+        end
+
+        -- Enable highlighting
+        pcall(vim.treesitter.start)
+      end,
     })
   end,
 }
