e07d04d0493daa1c8f563d12c8e42fa798155db0
joshw34 <joshwhitley@msn.com>
2025-12-16 00:14:19 +0100

    refixed treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,32 +2,30 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
+    local ts = require('nvim-treesitter')
+    ts.setup {
+      install_dir = vim.fn.stdpath('data') .. '/site'
+    }
+    -- Install parsers upfront
+    ts.install({
+      'bash',
+      'c',
+      'cpp',
+      'lua',
+      'python',
+      'dockerfile',
+      'nginx',
+      'go',
+      'html',
+      'vim',
+      'vimdoc',
+      'nix',
+    })
+
+    -- Auto-enable highlighting
     vim.api.nvim_create_autocmd('FileType', {
       pattern = '*',
       callback = function()
-        local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
-        if not lang then
-          return
-        end
-
-        -- Check if already installed
-        local installed = vim.tbl_contains(
-          require('nvim-treesitter.info').installed_parsers(),
-          lang
-        )
-
-        -- Check if parser is available for installation
-        local available = vim.tbl_contains(
-          require('nvim-treesitter.parsers').available_parsers(),
-          lang
-        )
-
-        -- Only install if available and not installed
-        if available and not installed then
-          vim.cmd("TSInstall " .. lang)
-        end
-
-        -- Enable highlighting
         pcall(vim.treesitter.start)
       end,
     })
